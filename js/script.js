d4775a93f05ff9308a2c72c2b4c87126ffb9c9d3
Oks100 <79767723+Oks100@users.noreply.github.com>
2022-08-22 18:11:18 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,114 +1,64 @@
-if (document.querySelector('.swiper-container')) {
-  new Swiper('.swiper-container', {
-    // Вывод стрелок навигации
-     navigation: {
-       nextEl: '.swiper-button-next',
-       prevEl: '.swiper-button-prev',
-     },
-   pagination: {
-     el: '.swiper-pagination',
+new Swiper('.swiper-container', {
+  // Вывод стрелок навигации
+   navigation: {
+     nextEl: '.swiper-button-next',
+     prevEl: '.swiper-button-prev',
    },
-     // Вывод скроллбара
-   scrollbar: {
-     el: '.swiper-scrollbar',
-      draggable: true,
-   },
-     // Прогрессбар
-    pagination: {
-        el: '.swiper-pagination',
-        type: 'progressbar'
-    },
-    loop: true,
-    preloadImages: false,
-    // Lazy Loading (подзагрузка картинок)
-    lazy: {
-      // Подзагрузка на старте перключения слайда
-      loadOnTransitionStart: true,
-      // Подгрузить предыдущую и следующую картинки
-      loadPrevNext: true,
-    },
-    effect: 'flip',
-    flipEffect: {
-      // Тень
-      slideShadows: true,
-      // Показ только активного слайда
-      limitRotation: true,
-    },
-   })
-};
+ pagination: {
+   el: '.swiper-pagination',
+ },
+   // Вывод скроллбара
+ scrollbar: {
+   el: '.swiper-scrollbar',
+    draggable: true,
+ },
+   // Прогрессбар
+  pagination: {
+      el: '.swiper-pagination',
+      type: 'progressbar'
+  },
+  loop: true,
+  preloadImages: false,
+  // Lazy Loading (подзагрузка картинок)
+  lazy: {
+    // Подзагрузка на старте перключения слайда
+    loadOnTransitionStart: true,
+    // Подгрузить предыдущую и следующую картинки
+    loadPrevNext: true,
+  },
+  effect: 'flip',
+  flipEffect: {
+    // Тень
+    slideShadows: true,
+    // Показ только активного слайда
+    limitRotation: true,
+  },
+ });
+
+/*$(document).ready(function() {
+    $('.header__burger').click(function(event){
+        $('.header__burger,.header__menu').toggleClass('active');
+        $('body').toggleClass('lock');
+    })
+});
+
+const checkbox = document.getElementById('checkbox');
+
+checkbox.addEventListener('change', ()=>{
+  document.body.classList.toggle('dark');
+});*/
 
 
-
-if (document.querySelector('.swiper-container2')) {
-	new Swiper('.swiper-container2', {
-		slidesPerView: 1,
-		spaceBetween: 5,
-		loop: true,
-		pagination: {
-			el: '.swiper-pagination',
-			clickable: true,
-		},
-		navigation: {
-			nextEl: '.swiper-button-next',
-			prevEl: '.swiper-button-prev',
-		},
-    autoplay: {
-      delay: 5000,
-    },
-    // effect: 'creative',
-    // creativeEffect: {
-    //   limitProgress: 3,
-    //   prev: {
-    //     translate: [0, 0, -400],
-    //   },
-    //   next: {
-    //     translate: ['100%', 0, 0],
-    //   },
-    // },
-		breakpoints: {
-			1025: {
-				slidesPerView: 3,
-			},
-			500: {
-				slidesPerView: 1,
-			}
-		}
-	})
-}
-
-/*const slider = document.querySelector('.slider-container3');
-
-let mySwiper;
-
-function mobileSlider() {
-	if (window.innerWidth <= 600 && slider.dataset.mobile == 'false') {
-		mySwiper = new Swiper(slider, {
-			slidesPerView: 1,
-			spaceBetween: 0,
-			loop: true,
-      flipEffect: {
-        slideShadows: false,
-      },
-			slideClass: 'card',
-			pagination: {
-			el: '.swiper-pagination',
-			clickable: true,
-			},
-		});
-
-		slider.dataset.mobile = 'true';
-	}
-
-	if (window.innerWidth > 600) {
-		slider.dataset.mobile = 'false';
-		if (slider.classList.contains('swiper-initialized')) {
-			mySwiper.destroy();
-		}
-	}
-}
-
-mobileSlider()
-
-window.addEventListener('resize', () => {
-	mobileSlider();
+/*const swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'progressbar'
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 });*/
