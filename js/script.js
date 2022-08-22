3bb03dcdf5ca52f70b2140fa4f59e5317b51c61f
Oks100 <79767723+Oks100@users.noreply.github.com>
2022-08-22 18:57:01 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,64 +1,77 @@
-new Swiper('.swiper-container', {
-  // Вывод стрелок навигации
-   navigation: {
-     nextEl: '.swiper-button-next',
-     prevEl: '.swiper-button-prev',
+if (document.querySelector('.swiper-container')) {
+  new Swiper('.swiper-container', {
+    // Вывод стрелок навигации
+     navigation: {
+       nextEl: '.swiper-button-next',
+       prevEl: '.swiper-button-prev',
+     },
+   pagination: {
+     el: '.swiper-pagination',
    },
- pagination: {
-   el: '.swiper-pagination',
- },
-   // Вывод скроллбара
- scrollbar: {
-   el: '.swiper-scrollbar',
-    draggable: true,
- },
-   // Прогрессбар
-  pagination: {
-      el: '.swiper-pagination',
-      type: 'progressbar'
-  },
-  loop: true,
-  preloadImages: false,
-  // Lazy Loading (подзагрузка картинок)
-  lazy: {
-    // Подзагрузка на старте перключения слайда
-    loadOnTransitionStart: true,
-    // Подгрузить предыдущую и следующую картинки
-    loadPrevNext: true,
-  },
-  effect: 'flip',
-  flipEffect: {
-    // Тень
-    slideShadows: true,
-    // Показ только активного слайда
-    limitRotation: true,
-  },
- });
-
-/*$(document).ready(function() {
-    $('.header__burger').click(function(event){
-        $('.header__burger,.header__menu').toggleClass('active');
-        $('body').toggleClass('lock');
-    })
-});
-
-const checkbox = document.getElementById('checkbox');
-
-checkbox.addEventListener('change', ()=>{
-  document.body.classList.toggle('dark');
-});*/
+     // Вывод скроллбара
+   scrollbar: {
+     el: '.swiper-scrollbar',
+      draggable: true,
+   },
+     // Прогрессбар
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'progressbar'
+    },
+    loop: true,
+    preloadImages: false,
+    // Lazy Loading (подзагрузка картинок)
+    lazy: {
+      // Подзагрузка на старте перключения слайда
+      loadOnTransitionStart: true,
+      // Подгрузить предыдущую и следующую картинки
+      loadPrevNext: true,
+    },
+    effect: 'flip',
+    flipEffect: {
+      // Тень
+      slideShadows: true,
+      // Показ только активного слайда
+      limitRotation: true,
+    },
+   })
+};
 
 
-/*const swiper = new Swiper('.swiper', {
-  direction: 'horizontal',
-  loop: true,
-  slidesPerView: 1,
-  pagination: {
-    el: '.swiper-pagination',
-    type: 'progressbar'
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-});*/
+
+if (document.querySelector('.swiper-container2')) {
+	new Swiper('.swiper-container2', {
+		slidesPerView: 1,
+		spaceBetween: 5,
+		loop: true,
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true,
+		},
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+    autoplay: {
+      delay: 5000,
+    },
+    // effect: 'creative',
+    // creativeEffect: {
+    //   limitProgress: 3,
+    //   prev: {
+    //     translate: [0, 0, -400],
+    //   },
+    //   next: {
+    //     translate: ['100%', 0, 0],
+    //   },
+    // },
+		breakpoints: {
+			1025: {
+				slidesPerView: 3,
+			},
+			500: {
+				slidesPerView: 1,
+			}
+		}
+	})
+}
