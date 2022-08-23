61bfec96273c31a89cc560a611838dab5dabcd1e
Oks100 <79767723+Oks100@users.noreply.github.com>
2022-08-23 17:28:31 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,114 @@
+if (document.querySelector('.swiper-container')) {
+  new Swiper('.swiper-container', {
+    // Вывод стрелок навигации
+     navigation: {
+       nextEl: '.swiper-button-next',
+       prevEl: '.swiper-button-prev',
+     },
+   pagination: {
+     el: '.swiper-pagination',
+   },
+     // Вывод скроллбара
+   scrollbar: {
+     el: '.swiper-scrollbar',
+      draggable: true,
+   },
+     // Прогрессбар
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'progressbar'
+    },
+    loop: true,
+    preloadImages: false,
+    // Lazy Loading (подзагрузка картинок)
+    lazy: {
+      // Подзагрузка на старте перключения слайда
+      loadOnTransitionStart: true,
+      // Подгрузить предыдущую и следующую картинки
+      loadPrevNext: true,
+    },
+    effect: 'flip',
+    flipEffect: {
+      // Тень
+      slideShadows: true,
+      // Показ только активного слайда
+      limitRotation: true,
+    },
+   })
+};
+
+
+
+if (document.querySelector('.swiper-container2')) {
+	new Swiper('.swiper-container2', {
+		slidesPerView: 1,
+		spaceBetween: 5,
+		loop: true,
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true,
+		},
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+    autoplay: {
+      delay: 5000,
+    },
+    // effect: 'creative',
+    // creativeEffect: {
+    //   limitProgress: 3,
+    //   prev: {
+    //     translate: [0, 0, -400],
+    //   },
+    //   next: {
+    //     translate: ['100%', 0, 0],
+    //   },
+    // },
+		breakpoints: {
+			1025: {
+				slidesPerView: 3,
+			},
+			500: {
+				slidesPerView: 1,
+			}
+		}
+	})
+}
+
+/*const slider = document.querySelector('.slider-container3');
+
+let mySwiper;
+
+function mobileSlider() {
+	if (window.innerWidth <= 600 && slider.dataset.mobile == 'false') {
+		mySwiper = new Swiper(slider, {
+			slidesPerView: 1,
+			spaceBetween: 0,
+			loop: true,
+      flipEffect: {
+        slideShadows: false,
+      },
+			slideClass: 'card',
+			pagination: {
+			el: '.swiper-pagination',
+			clickable: true,
+			},
+		});
+
+		slider.dataset.mobile = 'true';
+	}
+
+	if (window.innerWidth > 600) {
+		slider.dataset.mobile = 'false';
+		if (slider.classList.contains('swiper-initialized')) {
+			mySwiper.destroy();
+		}
+	}
+}
+
+mobileSlider()
+
+window.addEventListener('resize', () => {
+	mobileSlider();
+});*/
