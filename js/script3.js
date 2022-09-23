a32c3405f2021049258bbd4214c7335ab9c2e39d
Oks100 <79767723+Oks100@users.noreply.github.com>
2022-09-23 14:34:05 +0300

    Add files via upload

---
+++ js/script3.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+   $('.category__one').click(function(event) {
+          $('.vertical-menu-block__list-one').toggleClass('active');
+   });
+   $('.category__two').click(function(event) {
+    $('.vertical-menu-block__list-two').toggleClass('active');
+  });
+  $('.category__three').click(function(event) {
+    $('.vertical-menu-block__list-three').toggleClass('active');
+  });
+  $('.category__four').click(function(event) {
+    $('.vertical-menu-block__list-four').toggleClass('active');
+  });
+ });
+
+ $(document).ready(function(){
+  $(".header__notifications").click(function(event){
+    $(".header__notifications_conteiner").toggleClass("notif-more");
+  });
+});
