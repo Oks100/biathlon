ec00f830a1bd7bf234a3ff420c034ffea77156af
Oks100 <79767723+Oks100@users.noreply.github.com>
2022-12-02 22:05:49 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1375,3 +1375,25 @@ function burgerMenu(selector) {
 
 burgerMenu('.burger-menu');
 /* Бургер кнопка */
+
+
+/* Слайд меню для бургера*/
+$(document).ready(function(){
+  $('.burger-menu_nav-more').click(function () {
+    $('.burger-menu_nav-menu-content').toggleClass('new-slide');
+    });
+  });
+
+
+$(document).ready(function(){
+  $('.burger-menu-back-tab').click(function () {
+    $('.burger-menu_nav-menu-content').toggleClass('new-slide');
+      });
+  });
+
+$(document).ready(function(){
+  $('.burger-menu__layer-background').click(function () {
+    $('.burger-menu_nav-menu-content').toggleClass('new-slide');
+      });
+  });
+/* Слайд меню для бургера*/
