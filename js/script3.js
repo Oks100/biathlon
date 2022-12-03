94701b369258e09c4052bfcfd22037c7dc9eab6f
Oks100 <79767723+Oks100@users.noreply.github.com>
2022-12-03 22:25:43 +0200

    Add files via upload

--- js/script3.js
+++ js/script3.js
@@ -1,18 +1,37 @@
 $(document).ready(function(){
-    $('.burger-menu_nav-more').click(function () {
-      $('.burger-menu_nav-menu-content').toggleClass('new-slide');
-      });
+    $('.more1').click(function () {
+        $('.burger-menu_nav-menu-content').toggleClass('new-slide');
+             });
     });
   
   
-  $(document).ready(function(){
-    $('.burger-menu-back-tab').click(function () {
-      $('.burger-menu_nav-menu-content').toggleClass('new-slide');
-        });
+$(document).ready(function(){
+    $('.burger-menu-back-tab.slide1').click(function () {
+        $('.burger-menu_nav-menu-content').toggleClass('new-slide');
+            });
     });
   
-  $(document).ready(function(){
+$(document).ready(function(){
     $('.burger-menu__layer-background').click(function () {
-      $('.burger-menu_nav-menu-content').toggleClass('new-slide');
-        });
+        $('.burger-menu_nav-menu-content').removeClass('new-slide');
+            });
+    });
+
+ $(document).ready(function(){
+    $('.more2').click(function () {
+        $('.burger-menu_nav-menu-content').toggleClass('new-slide2');
+            });
+    });
+      
+      
+$(document).ready(function(){
+    $('.burger-menu-back-tab.slide2').click(function () {
+        $('.burger-menu_nav-menu-content').toggleClass('new-slide2');
+            });
+    });
+      
+$(document).ready(function(){
+    $('.burger-menu__layer-background').click(function () {
+        $('.burger-menu_nav-menu-content').removeClass('new-slide2');
+            });
     });
