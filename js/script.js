bfc0d033dc237d070d224f05cf417832ddc08503
Oks100 <79767723+Oks100@users.noreply.github.com>
2022-12-03 20:26:52 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1378,22 +1378,22 @@ burgerMenu('.burger-menu');
 
 
 /* Слайд меню для бургера*/
-$(document).ready(function(){
-  $('.burger-menu_nav-more').click(function () {
-    $('.burger-menu_nav-menu-content').toggleClass('new-slide');
-    });
-  });
+// $(document).ready(function(){
+//   $('.burger-menu_nav-more').click(function () {
+//     $('.burger-menu_nav-menu-content').toggleClass('new-slide');
+//     });
+//   });
 
 
-$(document).ready(function(){
-  $('.burger-menu-back-tab').click(function () {
-    $('.burger-menu_nav-menu-content').toggleClass('new-slide');
-      });
-  });
+// $(document).ready(function(){
+//   $('.burger-menu-back-tab').click(function () {
+//     $('.burger-menu_nav-menu-content').toggleClass('new-slide');
+//       });
+//   });
 
-$(document).ready(function(){
-  $('.burger-menu__layer-background').click(function () {
-    $('.burger-menu_nav-menu-content').toggleClass('new-slide');
-      });
-  });
+// $(document).ready(function(){
+//   $('.burger-menu__layer-background').click(function () {
+//     $('.burger-menu_nav-menu-content').toggleClass('new-slide');
+//       });
+//   });
 /* Слайд меню для бургера*/
