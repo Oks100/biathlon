a45f045addd2e662a78cd44dfde770329550b491
Oks100 <79767723+Oks100@users.noreply.github.com>
2022-12-03 22:49:39 +0200

    Add files via upload

--- js/script3.js
+++ js/script3.js
@@ -35,3 +35,41 @@ $(document).ready(function(){
         $('.burger-menu_nav-menu-content').removeClass('new-slide2');
             });
     });
+
+$(document).ready(function(){
+    $('.more3').click(function () {
+        $('.burger-menu_nav-menu-content').toggleClass('new-slide3');
+            });
+    });
+          
+          
+$(document).ready(function(){
+    $('.burger-menu-back-tab.slide3').click(function () {
+        $('.burger-menu_nav-menu-content').toggleClass('new-slide3');
+            });
+    });
+          
+$(document).ready(function(){
+    $('.burger-menu__layer-background').click(function () {
+        $('.burger-menu_nav-menu-content').removeClass('new-slide3');
+            });
+    });
+
+$(document).ready(function(){
+    $('.more4').click(function () {
+        $('.burger-menu_nav-menu-content').toggleClass('new-slide4');
+            });
+    });
+              
+              
+$(document).ready(function(){
+    $('.burger-menu-back-tab.slide4').click(function () {
+        $('.burger-menu_nav-menu-content').toggleClass('new-slide4');
+            });
+    });
+              
+$(document).ready(function(){
+    $('.burger-menu__layer-background').click(function () {
+        $('.burger-menu_nav-menu-content').removeClass('new-slide4');
+            });
+    });
