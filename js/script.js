cd8b0165925bcfde4a7e4f0fe613c1c9b7e9c618
Oks100 <79767723+Oks100@users.noreply.github.com>
2022-12-05 18:12:23 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,63 @@
+
+/* Табы*/
+$('.burger-menu__tab').click(function() {
+  var id = $(this).attr('data-tab'),
+      content = $('.burger-menu_nav-menu-list2[data-tab="'+ id +'"]');
+  
+  $('.burger-menu__tab.active').removeClass('active');
+  $(this).addClass('active'); // 2
+  
+  $('.burger-menu_nav-menu-list2.active').removeClass('active');
+  content.addClass('active');
+});
+/* Табы*/
+
+/* Бургер кнопка */
+function burgerMenu(selector) {
+  let menu = $(selector);
+  let button = menu.find('.burger-menu_button', '.burger-menu_lines');
+  let links = menu.find('.burger-menu_link');
+  // let overlay = menu.find('.burger-menu_overlay');
+  
+  button.on('click', (e) => {
+    e.preventDefault();
+    toggleMenu();
+  });
+  
+  links.on('click', () => toggleMenu());
+  // overlay.on('click', () => toggleMenu());
+  
+
+  function toggleMenu(){
+    menu.toggleClass('burger-menu_active');
+    
+    if (menu.hasClass('burger-menu_active')) {
+      $('body').css('overlow', 'hidden');
+    } else {
+      $('body').css('overlow', 'visible');
+    }
+  }
+  $(document).ready(function(){
+    $('.burger-menu_button').click(function () {
+      $('.body').toggleClass('overflow');
+      });
+    });
+  
+    $(document).ready(function(){
+      $('.burger-menu_link').click(function () {
+        $('.body').toggleClass('overflow');
+        });
+      });
+}
+
+burgerMenu('.burger-menu');
+/* Бургер кнопка */
+
+
+
+
+
+
 if (document.querySelector('.swiper-container5')) {
   new Swiper('.swiper-container5', {
     spaceBetween: 15,
@@ -1322,59 +1382,6 @@ btn.on('click', function(e) {
 //     });
 /* Костыль к бургер кнопке*/
 
-/* Табы*/
-$('.burger-menu__tab').click(function() {
-  var id = $(this).attr('data-tab'),
-      content = $('.burger-menu_nav-menu-list2[data-tab="'+ id +'"]');
-  
-  $('.burger-menu__tab.active').removeClass('active');
-  $(this).addClass('active'); // 2
-  
-  $('.burger-menu_nav-menu-list2.active').removeClass('active');
-  content.addClass('active');
-});
-/* Табы*/
-
-/* Бургер кнопка */
-function burgerMenu(selector) {
-  let menu = $(selector);
-  let button = menu.find('.burger-menu_button', '.burger-menu_lines');
-  let links = menu.find('.burger-menu_link');
-  // let overlay = menu.find('.burger-menu_overlay');
-  
-  button.on('click', (e) => {
-    e.preventDefault();
-    toggleMenu();
-  });
-  
-  links.on('click', () => toggleMenu());
-  // overlay.on('click', () => toggleMenu());
-  
-
-  function toggleMenu(){
-    menu.toggleClass('burger-menu_active');
-    
-    if (menu.hasClass('burger-menu_active')) {
-      $('body').css('overlow', 'hidden');
-    } else {
-      $('body').css('overlow', 'visible');
-    }
-  }
-  $(document).ready(function(){
-    $('.burger-menu_button').click(function () {
-      $('.body').toggleClass('overflow');
-      });
-    });
-  
-    $(document).ready(function(){
-      $('.burger-menu_link').click(function () {
-        $('.body').toggleClass('overflow');
-        });
-      });
-}
-
-burgerMenu('.burger-menu');
-/* Бургер кнопка */
 
 
 /* Слайд меню для бургера*/
