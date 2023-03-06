c8b8e335731891a54effd39a8a73c71c19930577
Oks100 <79767723+Oks100@users.noreply.github.com>
2023-03-06 16:36:44 +0200

    Add files via upload

--- js/script3.js
+++ js/script3.js
@@ -296,56 +296,48 @@ $(document).ready(function(){
         });
     });
 
-/* Спойлер */
-    $(document).ready(function() {
-        $('.important-news__news-spoiler').click(function(event) {
-            $(this).toggleClass('active').next().slideToggle(300);
-        });
-    });
-/* Спойлер */
 
-/*Кнопка топ 10 */
+$(document).ready(function(){
+    $('.learn-more__text2-href').hover(
+    function() {
+        $('.swiper-button-prev-icon').addClass('rotate');
+        },
+    function() {
+        $('.swiper-button-prev-icon').removeClass('rotate');
+        },
+    );
+    });
+      
     $(document).ready(function(){
-        $('.learn-more__text2-href').hover(
-         function() {
-            $('.swiper-button-prev-icon').addClass('rotate');
-          },
-          function() {
-            $('.swiper-button-prev-icon').removeClass('rotate');
-          },
-        );
-      });
-    
-      $(document).ready(function(){
-        $('.learn-more__text2-href').hover(
-        function() {
-            $('.swiper-button-next-icon').addClass('rotate');
-            },
-            function() {
-            $('.swiper-button-next-icon').removeClass('rotate');
-            },
-        );
-      });
-    
-      $(document).ready(function(){
-        $('.learn-more__text3-href').hover(
-         function() {
-            $('.swiper-button-prev-icon').addClass('rotate2');
-          },
-          function() {
-            $('.swiper-button-prev-icon').removeClass('rotate2');
-          },
-        );
-      });
-    
-      $(document).ready(function(){
-        $('.learn-more__text3-href').hover(
-        function() {
-            $('.swiper-button-next-icon').addClass('rotate2');
-            },
-            function() {
-            $('.swiper-button-next-icon').removeClass('rotate2');
-            },
-        );
-      });
-/*Кнопка топ 10 */
+    $('.learn-more__text2-href').hover(
+    function() {
+        $('.swiper-button-next-icon').addClass('rotate');
+        },
+    function() {
+        $('.swiper-button-next-icon').removeClass('rotate');
+        },
+    );
+    });
+      
+    $(document).ready(function(){
+    $('.learn-more__text3-href').hover(
+    function() {
+        $('.swiper-button-prev-icon').addClass('rotate2');
+        },
+    function() {
+        $('.swiper-button-prev-icon').removeClass('rotate2');
+        },
+    );
+    });
+      
+    $(document).ready(function(){
+    $('.learn-more__text3-href').hover(
+    function() {
+        $('.swiper-button-next-icon').addClass('rotate2');
+        },
+    function() {
+        $('.swiper-button-next-icon').removeClass('rotate2');
+        },
+    );
+    });
+      
