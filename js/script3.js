2ca5082441aa817d438f3b11594ef76826b3a679
Oks100 <79767723+Oks100@users.noreply.github.com>
2023-03-20 21:19:21 +0200

    Add files via upload

--- js/script3.js
+++ js/script3.js
@@ -585,3 +585,264 @@ $(document).ready(function(){
       }
       
       const calendarControl = new CalendarControl();
+
+
+
+
+      document.querySelector('.calendar-close-crosshair').addEventListener('click', () => {
+        document.querySelector('.timer__more_info').classList.remove('calendar-open');
+      });
+      document.querySelector('.calendar-body').addEventListener('click', () => {
+        document.querySelector('.timer__more_info').classList.remove('calendar-open');
+      });
+
+
+
+      function CalendarControl2() {
+        const calendar2 = new Date();
+        const calendarControl2 = {
+          localDate: new Date(),
+          prevMonthLastDate: null,
+          calWeekDays: ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс"],
+          calMonthName: [
+            "Январь",
+            "Фефраль",
+            "Март",
+            "Апрель",
+            "Май",
+            "Июнь",
+            "Июль",
+            "Август",
+            "Сентябрь",
+            "Октябрь",
+            "Ноябрь",
+            "Декабрь"
+          ],
+          daysInMonth: function (month, year) {
+            return new Date(year, month, 0).getDate();
+          },
+          firstDay: function () {
+            return new Date(calendar2.getFullYear(), calendar2.getMonth(), 1);
+          },
+          lastDay: function () {
+            return new Date(calendar2.getFullYear(), calendar2.getMonth() + 1, 0);
+          },
+          firstDayNumber: function () {
+            return calendarControl2.firstDay().getDay() + 1;
+          },
+          lastDayNumber: function () {
+            return calendarControl2.lastDay().getDay() + 1;
+          },
+          getPreviousMonthLastDate: function () {
+            let lastDate = new Date(
+              calendar2.getFullYear(),
+              calendar2.getMonth(),
+              0
+            ).getDate();
+            return lastDate;
+          },
+          navigateToPreviousMonth: function () {
+            calendar2.setMonth(calendar2.getMonth() - 1);
+            calendarControl2.attachEventsOnNextPrev();
+          },
+          navigateToNextMonth: function () {
+            calendar2.setMonth(calendar2.getMonth() + 1);
+            calendarControl2.attachEventsOnNextPrev();
+          },
+          navigateToCurrentMonth: function () {
+            let currentMonth = calendarControl2.localDate.getMonth();
+            let currentYear = calendarControl2.localDate.getFullYear();
+            calendar2.setMonth(currentMonth);
+            calendar2.setYear(currentYear);
+            calendarControl2.attachEventsOnNextPrev();
+          },
+          displayYear: function () {
+            let yearLabel = document.querySelector(".calendar2 .calendar-year-label");
+            yearLabel.innerHTML = calendar2.getFullYear();
+          },
+          displayMonth: function () {
+            let monthLabel = document.querySelector(
+              ".calendar2 .calendar-month-label"
+            );
+            monthLabel.innerHTML = calendarControl2.calMonthName[calendar2.getMonth()];
+          },
+          selectDate: function (e) {
+            console.log(
+              `${e.target.textContent} ${
+                calendarControl2.calMonthName[calendar2.getMonth()]
+              } ${calendar2.getFullYear()}`
+            );
+          },
+          plotSelectors: function () {
+            document.querySelector(
+              ".calendar2"
+            ).innerHTML += `<div class="calendar-inner">
+              <div class="calendar-controls">
+              <div class="calendar-year-month">
+              <div class="calendar-month-label"></div>
+              <div class="calendar-year-label"></div>
+              </div>
+              <div class="calendar-prev"><a href="#"><span class="calendar-prev-icon"></span></a></div>
+              <div class="calendar-next"><a href="#"><span class="calendar-next-icon"></span></a></div>
+              <div class="calendar-close-crosshair"></div>
+              </div>
+              <div class="calendar-today-date">Сегодня: 
+                ${calendarControl2.calWeekDays[calendarControl2.localDate.getDay()]}, 
+                ${calendarControl2.localDate.getDate()}, 
+                ${calendarControl2.calMonthName[calendarControl2.localDate.getMonth()]} 
+                ${calendarControl2.localDate.getFullYear()}
+              </div>
+              <div class="calendar-body"></div>
+              <div class="map-legend">
+              <span class="map-legend-legend">Легенда:</span>
+              <span class="map-legend-1">Кубок Мира/ЧМ/ОИ</span>
+              <span class="map-legend-2">Кубок IBU/ЧЕ</span>
+              <span class="map-legend-3">Юниорский Кубок IBU/ЮЧЕ/ЮЧМ</span>
+              </div>
+              </div>`;
+          },
+          plotDayNames: function () {
+            for (let i = 0; i < calendarControl2.calWeekDays.length; i++) {
+              document.querySelector(
+                ".calendar2 .calendar-body"
+              ).innerHTML += `<div>${calendarControl2.calWeekDays[i]}</div>`;
+            }
+          },
+          plotDates: function () {
+            document.querySelector(".calendar2 .calendar-body").innerHTML = "";
+            calendarControl2.plotDayNames();
+            calendarControl2.displayMonth();
+            calendarControl2.displayYear();
+            let count = 1;
+            let prevDateCount = 0;
+      
+            calendarControl2.prevMonthLastDate = calendarControl2.getPreviousMonthLastDate();
+            let prevMonthDatesArray = [];
+            let calendarDays = calendarControl2.daysInMonth(
+              calendar2.getMonth() + 1,
+              calendar2.getFullYear()
+            );
+            // dates of current month
+            for (let i = 1; i < calendarDays; i++) {
+              if (i < calendarControl2.firstDayNumber()) {
+                prevDateCount += 1;
+                document.querySelector(
+                  ".calendar2 .calendar-body"
+                ).innerHTML += `<div class="prev-dates"></div>`;
+                prevMonthDatesArray.push(calendarControl2.prevMonthLastDate--);
+              } else {
+                document.querySelector(
+                  ".calendar2 .calendar-body"
+                ).innerHTML += `<div class="number-item" data-num=${count}><span class="number-legend" data-num=${count}></span><a class="dateNumber" href="#">${count++}</a></div>`;
+              }
+            }
+            //remaining dates after month dates
+            for (let j = 0; j < prevDateCount + 1; j++) {
+              document.querySelector(
+                ".calendar2 .calendar-body"
+              ).innerHTML += `<div class="number-item" data-num=${count}><span class="number-legend" data-num=${count}></span><a class="dateNumber" href="#">${count++}</a></div>`;
+            }
+            calendarControl2.highlightToday();
+            calendarControl2.plotPrevMonthDates(prevMonthDatesArray);
+            calendarControl2.plotNextMonthDates();
+          },
+          attachEvents: function () {
+            let prevBtn = document.querySelector(".calendar2 .calendar-prev a");
+            let nextBtn = document.querySelector(".calendar2 .calendar-next a");
+            let todayDate = document.querySelector(".calendar2 .calendar-today-date");
+            let dateNumber = document.querySelectorAll(".calendar2 .dateNumber");
+            prevBtn.addEventListener(
+              "click",
+              calendarControl2.navigateToPreviousMonth
+            );
+            nextBtn.addEventListener("click", calendarControl2.navigateToNextMonth);
+            todayDate.addEventListener(
+              "click",
+              calendarControl2.navigateToCurrentMonth
+            );
+            for (var i = 0; i < dateNumber.length; i++) {
+                dateNumber[i].addEventListener(
+                  "click",
+                  calendarControl2.selectDate,
+                  false
+                );
+            }
+          },
+          highlightToday: function () {
+            let currentMonth = calendarControl2.localDate.getMonth() + 1;
+            let changedMonth = calendar2.getMonth() + 1;
+            let currentYear = calendarControl2.localDate.getFullYear();
+            let changedYear = calendar2.getFullYear();
+            if (
+              currentYear === changedYear &&
+              currentMonth === changedMonth &&
+              document.querySelectorAll(".number-item")
+            ) {
+              document
+                .querySelectorAll(".number-item")
+                [calendar2.getDate() - 1].classList.add("calendar-today");
+            }
+          },
+          plotPrevMonthDates: function(dates){
+            dates.reverse();
+            for(let i=0;i<dates.length;i++) {
+                if(document.querySelectorAll(".prev-dates")) {
+                    document.querySelectorAll(".prev-dates")[i].textContent = dates[i];
+                }
+            }
+          },
+          plotNextMonthDates: function(){
+           let childElemCount = document.querySelector('.calendar-body').childElementCount;
+           //7 lines
+           if(childElemCount > 42 ) {
+               let diff = 49 - childElemCount;
+               calendarControl2.loopThroughNextDays(diff);
+           }
+    
+           //6 lines
+           if(childElemCount > 35 && childElemCount <= 42 ) {
+            let diff = 42 - childElemCount;
+            calendarControl2.loopThroughNextDays(42 - childElemCount);
+           }
+    
+          },
+          loopThroughNextDays: function(count) {
+            if(count > 0) {
+                for(let i=1;i<=count;i++) {
+                    document.querySelector('.calendar-body').innerHTML += `<div class="next-dates">${i}</div>`;
+                }
+            }
+          },
+          attachEventsOnNextPrev: function () {
+            calendarControl2.plotDates();
+            calendarControl2.attachEvents();
+          },
+          init: function () {
+            calendarControl2.plotSelectors();
+            calendarControl2.plotDates();
+            calendarControl2.attachEvents();
+          }
+        };
+        calendarControl2.init();
+      }
+      
+      const calendarControl2 = new CalendarControl2();
+
+
+      $(document).ready(function(){
+        $('.calendar-mobile').click(function () {
+            $('.mobile-timer__more-events-window').addClass('calendar-open');
+            });
+        });
+
+        $(document).ready(function(){
+          $('.calendar-close-crosshair').click(function () {
+              $('.mobile-timer__more-events-window').removeClass('calendar-open');
+              });
+          });
+
+          $(document).ready(function(){
+            $('.number-item').click(function () {
+                $('.mobile-timer__more-events-window').removeClass('calendar-open');
+                });
+            });
