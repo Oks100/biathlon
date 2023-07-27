aec784febb7952155bb5908907713ee53877e90c
Oks100 <79767723+Oks100@users.noreply.github.com>
2023-07-27 22:12:14 +0300

    Add files via upload

--- js/script3.js
+++ js/script3.js
@@ -853,3 +853,328 @@ $(document).ready(function(){
                   $('.birthday-block__congratulations').addClass('vote');
                   });
               });
+
+
+              $(document).ready(function(){
+                $('.layer__chat-debates-tabs').click(function () {
+                    $('.layer__chat').addClass('onon');
+                    });
+                });
+
+
+
+                $(document).ready(function(){
+                  $('.timeline-my-comments__head').click(function () {
+                    $('.timeline-my-comments__head').addClass('votefff');
+                    });
+                  });
+                
+                
+
+
+/*Фильтры */
+                  // var button = document.getElementById("filter-button");
+                  // var container = document.getElementById("filter-container");
+                  // var input = document.querySelectorAll("input");
+                  
+                  // button.onclick = function (e) {
+                  //   e.stopPropagation();
+                  //   if (container.classList.contains("filters--active")) {
+                  //   container.classList.remove("filters--active");
+                  //   } else {
+                  //   container.classList.add("filters--active");
+                  //   }
+                  // };
+                  
+                  // container.onclick = function (e) {
+                  //   e.stopPropagation();
+                  // };
+                  
+                  // window.onclick = function () {
+                  //   container.classList.remove("filters--active");
+                  // };
+                  
+                  // console.log(input);
+                  
+                  // for (var i = 0; i < input.length; i++) {
+                  //   var currentInput = input[i];
+                  
+                  //   currentInput.onclick = function () {
+                  //   var isChecked = false;
+                  //   for (var j = 0; j < input.length; j++) {
+                  //     if (input[j].checked) {
+                  //     isChecked = true;
+                  //     break;
+                  //     }
+                  //   }
+                  
+                  //   if (isChecked) {
+                  //     button.classList.add("button--highlight");
+                  //   } else {
+                  //     button.classList.remove("button--highlight");
+                  //   }
+                  //   };
+                  // }
+/*Фильтры */
+
+var button = document.getElementById("filter-button");
+var container = document.getElementById("filter-container");
+
+button.onclick = function (e) {
+  e.stopPropagation();
+  if (container.classList.contains("filters--active")) {
+  container.classList.remove("filters--active");
+  } else {
+  container.classList.add("filters--active");
+  }
+};
+
+container.onclick = function (e) {
+  e.stopPropagation();
+};
+
+window.onclick = function () {
+  container.classList.remove("filters--active");
+};
+
+
+// var select = document.querySelector('select');
+// var filters = document.querySelector('.timeline-athletes__filters');
+
+// // Добавляем обработчик на изменение значения select
+// select.addEventListener('change', function() {
+//   // Получаем выбранную опцию
+//   var selectedOption = this.options[this.selectedIndex];
+  
+//   // Очищаем все классы у элемента .timeline-athletes__filters
+//   filters.classList.remove('one');
+//   filters.classList.remove('two');
+//   filters.classList.remove('three');
+
+//   // Если значение выбранной опции равно "chos"
+//   if (selectedOption.value === "chos") {
+//     // Добавляем класс "one" к элементу с классом "timeline-athletes__filters"
+//     filters.classList.add('one');
+//   } else if (selectedOption.value === "chos2") {
+//     // Добавляем класс "two" к элементу с классом "timeline-athletes__filters"
+//     filters.classList.add('two');
+//   } else if (selectedOption.value === "chos3") {
+//     // Добавляем класс "three" к элементу с классом "timeline-athletes__filters"
+//     filters.classList.add('three');
+//   }
+// });
+
+// // Получаем элемент с классом "timeline-athletes__filters"
+// const filters = document.querySelector('.timeline-athletes__filters');
+
+// // Получаем элементы select и добавляем обработчик события change
+// const select = document.querySelector('select');
+// select.addEventListener('change', (event) => {
+//   // Получаем выбранную опцию
+//   const selectedOption = event.target.value;
+
+//   if (selectedOption === 'chos') {
+//     // Добавляем класс "one" к элементу с классом "timeline-athletes__filters"
+//     filters.classList.add('one');
+//   }
+// });
+
+// // Получаем элемент с классом "timeline-athletes__filters"
+// const filters2 = document.querySelector('.timeline-athletes__filters');
+
+// // Получаем элементы select и добавляем обработчик события change
+// const select2 = document.querySelector('select');
+// select2.addEventListener('change', (event) => {
+//   // Получаем выбранную опцию
+//   const selectedOption = event.target.value;
+
+//   if (selectedOption === 'chos2') {
+//     // Добавляем класс "one" к элементу с классом "timeline-athletes__filters"
+//     filters2.classList.add('two');
+//   }
+// });
+
+// // Получаем элемент с классом "timeline-athletes__filters"
+// const filters = document.querySelector('.timeline-athletes__filters');
+
+// // Получаем элементы select и добавляем обработчик события change
+// const select = document.querySelector('select');
+// select.addEventListener('change', (event) => {
+//   // Получаем выбранную опцию
+//   const selectedOption = event.target.value;
+
+//   if (selectedOption === 'chos3') {
+//     // Добавляем класс "one" к элементу с классом "timeline-athletes__filters"
+//     filters.classList.add('three');
+//   }
+// });
+
+// Получаем элемент с классом "timeline-athletes__filters"
+const filterss = document.querySelector('.timeline-athletes__filters');
+
+// Получаем элементы select и добавляем обработчик события change
+const selectt = document.querySelector('#f1');
+selectt.addEventListener('change', (event) => {
+  // Получаем выбранную опцию
+  const selectedOption = event.target.value;
+
+  if (selectedOption === 'chos') {
+    // Добавляем класс "two" к элементу с классом "timeline-athletes__filters"
+    filterss.classList.add('one');
+  } else {
+    // Если выбрана другая опция, удаляем класс "two"
+    filterss.classList.remove('one');
+  }
+});
+
+// Получаем элемент с классом "timeline-athletes__filters"
+const filters = document.querySelector('.timeline-athletes__filters');
+
+// Получаем элементы select и добавляем обработчик события change
+const select = document.querySelector('#f2');
+select.addEventListener('change', (event) => {
+  // Получаем выбранную опцию
+  const selectedOption = event.target.value;
+
+  if (selectedOption === 'chos2') {
+    // Добавляем класс "two" к элементу с классом "timeline-athletes__filters"
+    filters.classList.add('two');
+  } else {
+    // Если выбрана другая опция, удаляем класс "two"
+    filters.classList.remove('two');
+  }
+});
+
+// Получаем элемент с классом "timeline-athletes__filters"
+const filtersss = document.querySelector('.timeline-athletes__filters');
+
+// Получаем элементы select и добавляем обработчик события change
+const selecttt = document.querySelector('#f3');
+selecttt.addEventListener('change', (event) => {
+  // Получаем выбранную опцию
+  const selectedOption = event.target.value;
+
+  if (selectedOption === 'chos3') {
+    // Добавляем класс "two" к элементу с классом "timeline-athletes__filters"
+    filtersss.classList.add('three');
+  } else {
+    // Если выбрана другая опция, удаляем класс "two"
+    filtersss.classList.remove('three');
+  }
+});
+
+// Получаем элемент с классом "timeline-athletes__filters"
+const filterssss = document.querySelector('.timeline-athletes__filters');
+
+// Получаем элементы select и добавляем обработчик события change
+const selectttt = document.querySelector('#f4');
+selectttt.addEventListener('change', (event) => {
+  // Получаем выбранную опцию
+  const selectedOption = event.target.value;
+
+  if (selectedOption === 'chos4') {
+    // Добавляем класс "two" к элементу с классом "timeline-athletes__filters"
+    filterssss.classList.add('four');
+  } else {
+    // Если выбрана другая опция, удаляем класс "two"
+    filterssss.classList.remove('four');
+  }
+});
+
+// setInterval(() => {
+//   const filterstwo = document.querySelector('.timeline-athletes__filters');
+//   const circleButton = document.querySelector('.circle-for-button');
+  
+//   if (filterstwo.classList.contains('four')) {
+//     circleButton.textContent = '1';
+//   }
+// }, 1);
+
+
+// setInterval(() => {
+//   const filtersw = document.querySelector('.timeline-athletes__filters');
+//   const circleButton = document.querySelector('.circle-for-button');
+//   const classList = filtersw.classList;
+  
+//   if (classList.contains('one') || classList.contains('two') || classList.contains('three') || classList.contains('four')) {
+//     let count = 0;
+    
+//     if (classList.contains('one')) {
+//       count++;
+//     }
+    
+//     if (classList.contains('two')) {
+//       count++;
+//     }
+    
+//     if (classList.contains('three')) {
+//       count++;
+//     }
+    
+//     if (classList.contains('four')) {
+//       count++;
+//     }
+    
+//     circleButton.textContent = count;
+//   }
+// }, 1);
+
+setInterval(() => {
+  const filters = document.querySelector('.timeline-athletes__filters');
+  const circleButton = document.querySelector('.circle-for-button');
+  const classList = filters.classList;
+  
+  if (!classList.contains('one') && !classList.contains('two') && !classList.contains('three') && !classList.contains('four')) {
+    circleButton.textContent = '';
+  }
+  else if (classList.contains('one') || classList.contains('two') || classList.contains('three') || classList.contains('four')) {
+    let count = 0;
+    
+    if (classList.contains('one')) {
+      count++;
+    }
+    
+    if (classList.contains('two')) {
+      count++;
+    }
+    
+    if (classList.contains('three')) {
+      count++;
+    }
+    
+    if (classList.contains('four')) {
+      count++;
+    }
+    
+    circleButton.textContent = count;
+  }
+}, 1);
+
+
+$(document).ready(function(){
+  $('.apply-button').click(function () {
+      $('.filters').removeClass('filters--active');
+          });
+  });
+
+//   const filtersq = document.querySelector('.filters');
+//   const timelineFiltersq = document.querySelector('.timeline-athletes__filters');
+//   const buttonq = document.querySelector('.filters-remove');
+  
+//   buttonq.addEventListener('click', () => {
+//     filtersq.classList.remove('filters--active');
+//     timelineFiltersq.className = 'timeline-athletes__filters';
+//   });
+
+
+const filtersq = document.querySelector('.filters');
+const timelineFiltersq = document.querySelector('.timeline-athletes__filters');
+
+const optionsq = document.querySelectorAll('#f4 option, #f3 option, #f2 option, #f1 option');
+const buttonq = document.querySelector('.filters-remove');
+
+buttonq.addEventListener('click', () => {
+  filtersq.classList.remove('filters--active');
+  timelineFiltersq.className = 'timeline-athletes__filters';
+  optionsq.forEach(option => option.selected = false);
+});
