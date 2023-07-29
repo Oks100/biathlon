f40b6524cbf2a48414055b9b97e0d44f41e75eab
Oks100 <79767723+Oks100@users.noreply.github.com>
2023-07-29 23:18:37 +0300

    Add files via upload

---
+++ js/general-info.js
@@ -0,0 +1,200 @@
+document.querySelector('.text-1-click').addEventListener('click', function() {
+  var timelineGeneralInfo = document.querySelector('.timeline-general-info');
+  for (var i = 0; i < timelineGeneralInfo.classList.length; i++) {
+    if (timelineGeneralInfo.classList[i] !== 'timeline-general-info') {
+      timelineGeneralInfo.classList.remove(timelineGeneralInfo.classList[i]);
+    }
+  }
+  timelineGeneralInfo.classList.add('text-1-show');
+});
+
+document.querySelector('.text-2-click').addEventListener('click', function() {
+    var timelineGeneralInfo = document.querySelector('.timeline-general-info');
+    for (var i = 0; i < timelineGeneralInfo.classList.length; i++) {
+      if (timelineGeneralInfo.classList[i] !== 'timeline-general-info') {
+        timelineGeneralInfo.classList.remove(timelineGeneralInfo.classList[i]);
+      }
+    }
+    timelineGeneralInfo.classList.add('text-2-show');
+});
+
+document.querySelector('.text-3-click').addEventListener('click', function() {
+    var timelineGeneralInfo = document.querySelector('.timeline-general-info');
+    for (var i = 0; i < timelineGeneralInfo.classList.length; i++) {
+      if (timelineGeneralInfo.classList[i] !== 'timeline-general-info') {
+        timelineGeneralInfo.classList.remove(timelineGeneralInfo.classList[i]);
+      }
+    }
+    timelineGeneralInfo.classList.add('text-3-show');
+});
+
+document.querySelector('.text-4-click').addEventListener('click', function() {
+    var timelineGeneralInfo = document.querySelector('.timeline-general-info');
+    for (var i = 0; i < timelineGeneralInfo.classList.length; i++) {
+      if (timelineGeneralInfo.classList[i] !== 'timeline-general-info') {
+        timelineGeneralInfo.classList.remove(timelineGeneralInfo.classList[i]);
+      }
+    }
+    timelineGeneralInfo.classList.add('text-4-show');
+});
+
+document.querySelector('.text-5-click').addEventListener('click', function() {
+    var timelineGeneralInfo = document.querySelector('.timeline-general-info');
+    for (var i = 0; i < timelineGeneralInfo.classList.length; i++) {
+      if (timelineGeneralInfo.classList[i] !== 'timeline-general-info') {
+        timelineGeneralInfo.classList.remove(timelineGeneralInfo.classList[i]);
+      }
+    }
+    timelineGeneralInfo.classList.add('text-5-show');
+});
+
+document.querySelector('.text-6-click').addEventListener('click', function() {
+    var timelineGeneralInfo = document.querySelector('.timeline-general-info');
+    for (var i = 0; i < timelineGeneralInfo.classList.length; i++) {
+      if (timelineGeneralInfo.classList[i] !== 'timeline-general-info') {
+        timelineGeneralInfo.classList.remove(timelineGeneralInfo.classList[i]);
+      }
+    }
+    timelineGeneralInfo.classList.add('text-6-show');
+});
+
+document.querySelector('.text-7-click').addEventListener('click', function() {
+    var timelineGeneralInfo = document.querySelector('.timeline-general-info');
+    for (var i = 0; i < timelineGeneralInfo.classList.length; i++) {
+      if (timelineGeneralInfo.classList[i] !== 'timeline-general-info') {
+        timelineGeneralInfo.classList.remove(timelineGeneralInfo.classList[i]);
+      }
+    }
+    timelineGeneralInfo.classList.add('text-7-show');
+});
+
+document.querySelector('.text-8-click').addEventListener('click', function() {
+    var timelineGeneralInfo = document.querySelector('.timeline-general-info');
+    for (var i = 0; i < timelineGeneralInfo.classList.length; i++) {
+      if (timelineGeneralInfo.classList[i] !== 'timeline-general-info') {
+        timelineGeneralInfo.classList.remove(timelineGeneralInfo.classList[i]);
+      }
+    }
+    timelineGeneralInfo.classList.add('text-8-show');
+});
+
+document.querySelector('.text-9-click').addEventListener('click', function() {
+    var timelineGeneralInfo = document.querySelector('.timeline-general-info');
+    for (var i = 0; i < timelineGeneralInfo.classList.length; i++) {
+      if (timelineGeneralInfo.classList[i] !== 'timeline-general-info') {
+        timelineGeneralInfo.classList.remove(timelineGeneralInfo.classList[i]);
+      }
+    }
+    timelineGeneralInfo.classList.add('text-9-show');
+});
+
+document.querySelector('.text-10-click').addEventListener('click', function() {
+    var timelineGeneralInfo = document.querySelector('.timeline-general-info');
+    for (var i = 0; i < timelineGeneralInfo.classList.length; i++) {
+      if (timelineGeneralInfo.classList[i] !== 'timeline-general-info') {
+        timelineGeneralInfo.classList.remove(timelineGeneralInfo.classList[i]);
+      }
+    }
+    timelineGeneralInfo.classList.add('text-10-show');
+});
+
+document.querySelector('.text-11-click').addEventListener('click', function() {
+    var timelineGeneralInfo = document.querySelector('.timeline-general-info');
+    for (var i = 0; i < timelineGeneralInfo.classList.length; i++) {
+      if (timelineGeneralInfo.classList[i] !== 'timeline-general-info') {
+        timelineGeneralInfo.classList.remove(timelineGeneralInfo.classList[i]);
+      }
+    }
+    timelineGeneralInfo.classList.add('text-11-show');
+});
+
+document.querySelector('.text-12-click').addEventListener('click', function() {
+    var timelineGeneralInfo = document.querySelector('.timeline-general-info');
+    for (var i = 0; i < timelineGeneralInfo.classList.length; i++) {
+      if (timelineGeneralInfo.classList[i] !== 'timeline-general-info') {
+        timelineGeneralInfo.classList.remove(timelineGeneralInfo.classList[i]);
+      }
+    }
+    timelineGeneralInfo.classList.add('text-12-show');
+});
+
+document.querySelector('.text-13-click').addEventListener('click', function() {
+    var timelineGeneralInfo = document.querySelector('.timeline-general-info');
+    for (var i = 0; i < timelineGeneralInfo.classList.length; i++) {
+      if (timelineGeneralInfo.classList[i] !== 'timeline-general-info') {
+        timelineGeneralInfo.classList.remove(timelineGeneralInfo.classList[i]);
+      }
+    }
+    timelineGeneralInfo.classList.add('text-13-show');
+});
+
+
+document.querySelector('.text-14-click').addEventListener('click', function() {
+    var timelineGeneralInfo = document.querySelector('.timeline-general-info');
+    for (var i = 0; i < timelineGeneralInfo.classList.length; i++) {
+      if (timelineGeneralInfo.classList[i] !== 'timeline-general-info') {
+        timelineGeneralInfo.classList.remove(timelineGeneralInfo.classList[i]);
+      }
+    }
+    timelineGeneralInfo.classList.add('text-14-show');
+});
+
+document.querySelector('.text-15-click').addEventListener('click', function() {
+    var timelineGeneralInfo = document.querySelector('.timeline-general-info');
+    for (var i = 0; i < timelineGeneralInfo.classList.length; i++) {
+      if (timelineGeneralInfo.classList[i] !== 'timeline-general-info') {
+        timelineGeneralInfo.classList.remove(timelineGeneralInfo.classList[i]);
+      }
+    }
+    timelineGeneralInfo.classList.add('text-15-show');
+});
+
+document.querySelector('.text-16-click').addEventListener('click', function() {
+    var timelineGeneralInfo = document.querySelector('.timeline-general-info');
+    for (var i = 0; i < timelineGeneralInfo.classList.length; i++) {
+      if (timelineGeneralInfo.classList[i] !== 'timeline-general-info') {
+        timelineGeneralInfo.classList.remove(timelineGeneralInfo.classList[i]);
+      }
+    }
+    timelineGeneralInfo.classList.add('text-16-show');
+});
+
+document.querySelector('.text-17-click').addEventListener('click', function() {
+    var timelineGeneralInfo = document.querySelector('.timeline-general-info');
+    for (var i = 0; i < timelineGeneralInfo.classList.length; i++) {
+      if (timelineGeneralInfo.classList[i] !== 'timeline-general-info') {
+        timelineGeneralInfo.classList.remove(timelineGeneralInfo.classList[i]);
+      }
+    }
+    timelineGeneralInfo.classList.add('text-17-show');
+});
+
+document.querySelector('.text-18-click').addEventListener('click', function() {
+    var timelineGeneralInfo = document.querySelector('.timeline-general-info');
+    for (var i = 0; i < timelineGeneralInfo.classList.length; i++) {
+      if (timelineGeneralInfo.classList[i] !== 'timeline-general-info') {
+        timelineGeneralInfo.classList.remove(timelineGeneralInfo.classList[i]);
+      }
+    }
+    timelineGeneralInfo.classList.add('text-18-show');
+});
+
+document.querySelector('.text-19-click').addEventListener('click', function() {
+    var timelineGeneralInfo = document.querySelector('.timeline-general-info');
+    for (var i = 0; i < timelineGeneralInfo.classList.length; i++) {
+      if (timelineGeneralInfo.classList[i] !== 'timeline-general-info') {
+        timelineGeneralInfo.classList.remove(timelineGeneralInfo.classList[i]);
+      }
+    }
+    timelineGeneralInfo.classList.add('text-19-show');
+});
+
+document.querySelector('.text-20-click').addEventListener('click', function() {
+    var timelineGeneralInfo = document.querySelector('.timeline-general-info');
+    for (var i = 0; i < timelineGeneralInfo.classList.length; i++) {
+      if (timelineGeneralInfo.classList[i] !== 'timeline-general-info') {
+        timelineGeneralInfo.classList.remove(timelineGeneralInfo.classList[i]);
+      }
+    }
+    timelineGeneralInfo.classList.add('text-20-show');
+});
