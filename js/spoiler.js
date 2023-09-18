eb27cfa840ff67094eb01381301e28b27b4b6d74
Oks100 <79767723+Oks100@users.noreply.github.com>
2023-09-18 19:28:52 +0300

    Add files via upload

--- js/spoiler.js
+++ js/spoiler.js
@@ -3,3 +3,20 @@ $(document).ready(function() {
         $(this).toggleClass('active').next().slideToggle(300);
     });
 });
+
+// $(document).ready(function() {
+//     $('.timeline-season__body-nav-title').click(function(event) {
+//         $(this).toggleClass('active').next().slideToggle(300);
+//     });
+// });
+$(document).ready(function() {
+    $('.timeline-season__body-nav-title').each(function() {
+      if (!$(this).hasClass('active')) {
+        $(this).next().hide();
+      }
+    });
+  
+    $('.timeline-season__body-nav-title').click(function(event) {
+      $(this).toggleClass('active').next().slideToggle(300);
+    });
+  });
